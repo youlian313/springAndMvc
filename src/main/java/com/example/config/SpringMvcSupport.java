167dd9da9c465d3b14c320b7c5d0f50cc6ca3fcb
youlian <home computer>
2022-06-05 18:39:10 +0800

    spring+springmvc+mybatis

--- src/main/java/com/example/config/SpringMvcSupport.java
+++ src/main/java/com/example/config/SpringMvcSupport.java
@@ -1,13 +1,24 @@
 package com.example.config;
 
+import com.example.controller.interceptor.ProjectInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 //mvc 拦截过滤
 @Configuration
 public class SpringMvcSupport extends WebMvcConfigurationSupport {
+    @Autowired
+    private ProjectInterceptor projectInterceptor;
+
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
        registry.addResourceHandler("/html/**").addResourceLocations("/html/");
+    }
+
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+       registry.addInterceptor(projectInterceptor).addPathPatterns("/heros","/heros/*");
     }
 }
